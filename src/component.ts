a008e912f287092f66000e52f9a11d4bc1a58c0f
HeinousTugboat <admin@heinousindustries.com>
2017-09-02 23:41:51 -0400

    feat(component): add some base component tests and coverage

--- src/component.ts
+++ src/component.ts
@@ -2,6 +2,7 @@
 
 export interface ComponentType {
     label: string;
+    list: Component[];
     new(...args: any[]): Component;
 }
 
@@ -9,21 +10,30 @@ export abstract class Component {
     static list: { [K: string]: ComponentType } = {};
     static types: Map<ComponentType, Component[]> = new Map;
     static Builder(label: string) {
-        Component.list[label] = class ComponentType extends Component {
+        if (Component.list[label]) {
+            return Component.list[label];
+        }
+        let newType = class ComponentType implements Component, ComponentType{
             static label: string = label;
+            static list: Component[] = [];
+            list: Component[];
             label: string = label;
             constructor(public num: number) {
-                super();
+                // super();
+                ComponentType.list.push(this);
                 // super(label);
             }
             public getComponentType(): string {
                 return this.label;
             }
         };
-        Component.types.set(Component.list[label], []);
-        return Component.list[label];
+        Component.list[label] = newType;
+        let newList = newType.list;
+        Component.types.set(newType, newList);
+        return newType;
     }
     abstract label: string;
     abstract getComponentType(): string;
+    abstract list: Component[];
     // constructor() {}
 }
--- test/component.spec.ts
+++ test/component.spec.ts
@@ -6,19 +6,42 @@ describe('Component', function() {
     it('should exist', function() {
         expect(Component).to.exist;
     })
+    describe('Builder', function() {
+        it('should be added to Component.list', function() {
+            const TestComponent = Component.Builder('test-component');
+            expect(Component.list).to.have.property('test-component');
+            expect(Component.list['test-component']).to.deep.equal(TestComponent);
+        });
+        it('should return existing type if label already exists', function() {
+            const TestComponent = Component.Builder('test-component');
+            expect(Component.Builder('test-component')).to.deep.equal(TestComponent);
+        })
+        it('should have a list of all instances of itself', function() {
+            const TestComponent = Component.Builder('test-component');
+            const test = new TestComponent(1);
+            expect(TestComponent).to.have.property('list');
+            expect(TestComponent.list[0]).to.deep.equal(test);
+        });
+        it('should have a map of types to arrays of instances', function() {
+            const TestComponent = Component.Builder('test-component');
+            const test = new TestComponent(1);
+            expect(Component).to.have.property('types');
+            expect(Component.types.has(TestComponent)).to.be.true;
+            const list = Component.types.get(TestComponent);
+            expect(list).to.exist;
+            if (!list) { throw new Error('lolwat?'); }
+            expect(list[0]).to.deep.equal(test);
+        })
+        it('should have a list of all systems it is attached to', function() {
+            expect(true).to.be.false;
+        });
+    })
 })
 describe('ComponentType', function() {
     let TestComponent: ComponentType;
     beforeEach(function() {
         TestComponent = Component.Builder('test-component');
     })
-    describe('constructor', function() {
-        it('should throw an error without a label');
-        it('should be added to ECS.Component.list');
-        it('should throw an error if label already exists');
-        it('should have a list of all instances of itself');
-        it('should have a list of all systems it is attached to');
-    });
     describe('instance', function() {
         let component: Component;
         beforeEach(function() {
