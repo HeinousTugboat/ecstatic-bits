7265978191c6b90a950660eb6f748636261cde05
HeinousTugboat <admin@heinousindustries.com>
2017-09-02 23:41:37 -0400

    feat(entity): add some base entity tests and coverage

--- src/entity.ts
+++ src/entity.ts
@@ -1,6 +1,25 @@
+import { Component } from './component';
+
 /**
  * Basic Entity. It's just a number.
  */
+
+interface ComponentMap {
+    [key: string]: Component;
+}
+
 export class Entity {
+    static id: number = 0;
+    static list: Map<number, Entity> = new Map;
+    static find() { }
+
     id: number;
+    components: ComponentMap = {};
+    constructor(public name: string = 'Unnamed Entity') {
+        this.id = Entity.id++;
+        Entity.list.set(this.id, this);
+    }
+    add() { }
+    remove() { }
+    toJSON() { }
 }
--- test/entity.spec.ts
+++ test/entity.spec.ts
@@ -7,16 +7,49 @@ describe('Entity', function() {
         expect(Entity).to.exist;
     })
     describe('constructor', function() {
-        it('should have a default name');
-        it('should have an id');
-        it('should set the name to what is passed to it');
-        it('should have a unique id after 5 instances');
-        it('should have a list of components');
-        it('should be added to Entity.list');
+        it('should have a default name', function(){
+            const entity = new Entity;
+            expect(entity).to.have.property('name');
+        });
+        it('should have an id', function() {
+            const entity = new Entity;
+            expect(entity).to.have.property('id');
+            expect(entity.id).to.be.a('number').greaterThan(0);
+        });
+        it('should set the name to what is passed to it', function() {
+            const entity = new Entity('Test-Entity');
+            expect(entity.name).to.equal('Test-Entity');
+        });
+        it('should have a unique id after 5 instances', function() {
+            const entities: Entity[] = [];
+            const ids: Set<Number> = new Set;
+            for (let i = 0; i < 5; i++) {
+                let entity = new Entity;
+                entities.push(entity);
+                ids.add(entity.id);
+            }
+            expect(ids.size).to.equal(5);
+
+        });
+        it('should have a list of components', function() {
+            const entity = new Entity;
+            expect(entity).to.have.property('components');
+        });
+        it('should be added to Entity.list', function() {
+            const entity = new Entity;
+            expect(Entity.list.get(entity.id)).to.deep.equal(entity);
+        });
     })
     describe('prototype', function() {
-        describe('addComponent', function() {
-            it('should exist');
+        let entity: Entity;
+        beforeEach(function(){
+            entity = new Entity('Test Entity');
+        })
+        describe('add', function() {
+            it('should exist', function() {
+                expect(entity).to.have.property('add');
+                expect(entity.add).to.be.a('function');
+            });
             it('should add given component');
             it('should add initialized component');
             it('should throw an error if no component given');
@@ -25,14 +58,20 @@ describe('Entity', function() {
             it('should work the same as generating a new component via EID');
         });
         describe('removeComponent', function() {
-            it('should exist');
+            it('should exist', function() {
+                expect(entity).to.have.property('remove');
+                expect(entity.remove).to.be.a('function');
+            });
             it('should remove given component from entity');
             it('should throw an error if no component given');
             it('should throw an error if component given not found on entity');
             it('should remove this entity from given component\'s list');
         });
-        describe('print', function() {
-            it('should exist');
+        describe('toJSON', function() {
+            it('should exist', function() {
+                expect(entity).to.have.property('toJSON');
+                expect(entity.toJSON).to.be.a('function');
+            });
             it('should return a stringified version of entity');
             it('should throw an error if unable to serialize');
             it('should be able to be parsed back from JSON');
@@ -40,6 +79,9 @@ describe('Entity', function() {
         });
     });
     describe('findEntity', function() {
-        it('should exist');
+        it('should exist', function() {
+            expect(Entity).to.have.property('find');
+            expect(Entity.find).to.be.a('function');
+        });
     });
 })
