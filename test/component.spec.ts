d3970cdcfa87f4573792a23dd54d32def6c5f4dd
HeinousTugboat <admin@heinousindustries.com>
2017-09-27 14:48:30 -0400

    test: updating tests for component, including new decorators

--- test/component.spec.ts
+++ test/component.spec.ts
@@ -1,65 +1,115 @@
 import { expect } from 'chai';
 import 'mocha';
-import { Component, ComponentType } from '../src/component';
+import { Component, ComponentType, ComponentTypes } from '../src/component';
 
 describe('Component', function() {
     it('should exist', function() {
         expect(Component).to.exist;
     });
     describe('Builder', function() {
-        it('should be added to Component.list', function() {
-            const TestComponent = Component.Builder('test-component');
-            expect(Component.list).to.have.property('test-component');
-            expect(Component.list['test-component']).to.deep.equal(TestComponent);
+        let BuiltTestComponent: ComponentType;
+        beforeEach(function() {
+            BuiltTestComponent = Component.Builder('built-test-component');
+            BuiltTestComponent.prototype.initialize = () => {};
+        });
+        it('should be added to ComponentTypes', function() {
+            expect(ComponentTypes).to.have.property('built-test-component');
+            expect(ComponentTypes['built-test-component']).to.deep.equal(BuiltTestComponent);
         });
         it('should return existing type if label already exists', function() {
-            const TestComponent = Component.Builder('test-component');
-            expect(Component.Builder('test-component')).to.deep.equal(TestComponent);
+            expect(Component.Builder('built-test-component')).to.deep.equal(BuiltTestComponent);
         });
         it('should have a list of all instances of itself', function() {
-            const TestComponent = Component.Builder('test-component');
-            const test = new TestComponent(1);
-            expect(TestComponent).to.have.property('list');
-            expect(TestComponent.list[0]).to.deep.equal(test);
+            const test = new BuiltTestComponent(1);
+            expect(BuiltTestComponent).to.have.property('list');
+            expect(BuiltTestComponent.list[0]).to.deep.equal(test);
         });
         it('should have a map of types to arrays of instances', function() {
-            const TestComponent = Component.Builder('test-component');
-            const test = new TestComponent(1);
+            const test = new BuiltTestComponent(1);
             expect(Component).to.have.property('types');
-            expect(Component.types.has(TestComponent)).to.be.true;
-            const list = Component.types.get(TestComponent);
+            expect(Component.types.has(BuiltTestComponent)).to.be.true;
+            const list = Component.types.get(BuiltTestComponent);
             expect(list).to.exist;
             if (!list) { throw new Error('lolwat?'); }
             expect(list[0]).to.deep.equal(test);
         });
-        it('should have a list of all systems it is attached to', function() {
-            expect(true).to.be.false;
+        it('should have getComponentType()', function() {
+            const test = new BuiltTestComponent(1);
+            const str = test.getComponentType();
+            expect(str).to.equal('built-test-component');
+        });
+        // it('should have a list of all systems it is attached to', function() {
+        //     expect(true).to.be.false;
+        // });
+    });
+    describe('Class', function() {
+        @ComponentType('class-test-component')
+        class ClassTestComponent extends Component {
+            constructor(public eid: number) {
+                super(eid);
+            }
+        }
+
+        it('should be added to Component.list', function() {
+            expect(ComponentTypes).to.have.property('test-component');
+            expect(ComponentTypes['class-test-component']).to.deep.equal(ClassTestComponent.prototype.constructor);
+        });
+        it('should warn if using default initialize implementation', function() {
+            const component = new ClassTestComponent(3);
+            expect(function() {
+                component.initialize();
+            }).to.throw();
         });
     });
 });
 describe('ComponentType', function() {
-    let TestComponent: ComponentType;
+    @ComponentType('type-test-component')
+    class TypeTestComponent extends Component {
+        constructor(public eid: number) {
+            super(eid);
+        }
+        initialize() {}
+    }
     beforeEach(function() {
-        TestComponent = Component.Builder('test-component');
-    });
+        // TODO: set up Sinon sandbox for ComponentType?
+     });
     describe('instance', function() {
         let component: Component;
         beforeEach(function() {
-            component = new TestComponent();
+            component = new TypeTestComponent(1);
         });
-        it('should have an initialize function');
+        it('should have an initialize function', function() {
+            expect(component.initialize).to.exist;
+            expect(component.initialize).to.be.a('Function');
+        });
         describe('constructor', function() {
-            it('should generate a component instance');
-            it('should throw an error if no EID is given');
-            it('should throw an error if unable to locate EID given');
-            it('should throw an error if component already attached to Entity');
-            it('should register itself on ComponentType.list');
-            it('should register itself on the EID passed');
-            it('should call its own initialize function when generated');
+            it('should generate a component instance', function() {
+                expect(component).to.be.instanceof(TypeTestComponent);
+            });
+            it('should register itself on ComponentType.list', function() {
+                expect(TypeTestComponent.list).to.include(component);
+            });
+            it('should throw an error if duplicate component found', function(){
+                expect(function(){
+                    @ComponentType('type-test-component')
+                    class TestComponentNew extends Component {
+                        constructor(eid: number) {
+                            super(eid);
+                        }
+                    }
+                }).to.throw();
+            });
+            // it('should throw an error if unable to locate EID given');
+            // it('should throw an error if component already attached to Entity');
+            // it('should register itself on the EID passed');
+            // it('should call its own initialize function when generated', function() {
+            //     // TODO: set up Sinon to stub out initialize..
+            //     expect(true).to.be.false;
+            // });
         });
         it('should have getComponentType()', function() {
             const str = component.getComponentType();
-            expect(str).to.equal('test-component');
+            expect(str).to.equal('type-test-component');
         });
     });
 });
