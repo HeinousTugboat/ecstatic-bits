27188513567eaa96837fd9f6d3a9641b099809bc
HeinousTugboat <admin@heinousindustries.com>
2017-10-01 23:02:00 -0400

    test: random test file to verify everything works together properly

---
+++ src/test.system.ts
@@ -0,0 +1,42 @@
+import { Component, ComponentType } from './component';
+import { Entity } from './entity';
+import { System } from './system';
+
+@ComponentType('active')
+class ActiveComponent extends Component {
+    static primary: Entity | undefined;
+    private delay: number;
+    private command: {execute: (...args: any[]) => {}};
+    constructor(eid: number) {
+        super(eid);
+    }
+
+    makePrimary() {
+        ActiveComponent.primary = Entity.list.get(this.eid);
+    }
+    setDelay(delay: number) {
+        this.delay = delay;
+    }
+    execute(args: any[]) {
+        if (this.command && this.command.execute) {
+            return this.command.execute(args);
+        }
+    }
+}
+
+const InputSystem = new System('input', ActiveComponent, true);
+InputSystem.update = () => {
+    console.log('InputSystem update!');
+};
+
+InputSystem.hooks = {
+    LOAD_B64: () => {},
+    ACCEPT: () => {}
+};
+
+const Joe = new Entity('Joe Book');
+Joe.add('active');
+
+System.update(1);
+(<ActiveComponent>Joe.get('active')).makePrimary();
+console.log(ActiveComponent.primary);
